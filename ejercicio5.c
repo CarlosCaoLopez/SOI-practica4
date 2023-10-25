a26bcc2340a4a6fb02b9623eb8a90e63beddd6ef
CarlosCaoLopez <caolopezcarlos@gmail.com>
2023-10-25 14:16:10 +0200

    Inicio

---
+++ ejercicio5.c
@@ -0,0 +1,78 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sched.h>
+#include <math.h>
+
+#define NUMBER_OF_THREADS 17
+#define N 10000000
+
+typedef struct {
+
+    pthread_t tid;
+    
+}args;    
+
+
+void * thread_function(void *arguments){
+    args *aux;
+    int i;
+    float sum = 0.0;
+
+     aux = (args *) arguments;
+
+    if((aux->tid) == 0){/*Si estamos en el primer hilo, este cederá la CPU antes de cada iteración*/
+        printf("Soy el hilo generoso y mi tid es: %ld\n", aux->tid);
+        
+        for(i=0; i< N; i++){
+        
+            sched_yield();
+             
+            sum += sqrt(i);
+        }
+        printf("Soy el hilo generoso y he terminado. Mi tid es: %ld\n", aux->tid);
+        
+    }
+    else{ /* Hilos no generosos */
+        printf("Soy un hilo NO generoso y mi tid es: %ld\n", aux->tid);
+        for(i=0; i< N; i++){
+             
+            sum += sqrt(i);
+        }
+        
+        printf("Soy un hilo NO generoso y he terminado. Mi tid es: %ld\n", aux->tid);
+        
+    }
+    
+   
+    pthread_exit(NULL);
+
+}
+
+int main(int argc, char *argv[]){
+    args arguments;
+    pthread_t threads[NUMBER_OF_THREADS];
+    int status, i;
+    
+    
+    for(i=0; i < NUMBER_OF_THREADS; i++){
+        printf("Hilo principal. Creando hilo %d\n", i);
+        
+        arguments.tid = i; /* Guardamos el tid*/
+        
+        status = pthread_create(&threads[i], NULL, thread_function, (void *)&arguments);
+        
+        if(status != 0){
+            printf("Error en la creación del hilo. Status: %d\n", status);
+        }
+        sleep(1);
+    }
+    
+    for(i=0; i < NUMBER_OF_THREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+    
+
+    exit(EXIT_SUCCESS);
+}
