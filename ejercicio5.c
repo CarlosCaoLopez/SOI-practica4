3fc61e316755022390a8e9bd25f8639a5bac884e
CarlosCaoLopez <caolopezcarlos@gmail.com>
2023-11-08 12:23:44 +0100

    merda

--- ejercicio5.c
+++ ejercicio5.c
@@ -5,8 +5,8 @@
 #include <sched.h>
 #include <math.h>
 
-#define NUMBER_OF_THREADS 17
-#define N 10000000
+#define NUMBER_OF_THREADS 16
+#define N 1e9
 
 typedef struct {
 
@@ -14,54 +14,21 @@ typedef struct {
     
 }args;    
 
+void * thread_function(void *arguments);
 
-void * thread_function(void *arguments){
-    args *aux;
-    int i;
-    float sum = 0.0;
-
-     aux = (args *) arguments;
-
-    if((aux->tid) == 0){/*Si estamos en el primer hilo, este cederá la CPU antes de cada iteración*/
-        printf("Soy el hilo generoso y mi tid es: %ld\n", aux->tid);
-        
-        for(i=0; i< N; i++){
-        
-            sched_yield();
-             
-            sum += sqrt(i);
-        }
-        printf("Soy el hilo generoso y he terminado. Mi tid es: %ld\n", aux->tid);
-        
-    }
-    else{ /* Hilos no generosos */
-        printf("Soy un hilo NO generoso y mi tid es: %ld\n", aux->tid);
-        for(i=0; i< N; i++){
-             
-            sum += sqrt(i);
-        }
-        
-        printf("Soy un hilo NO generoso y he terminado. Mi tid es: %ld\n", aux->tid);
-        
-    }
-    
-   
-    pthread_exit(NULL);
-
-}
 
 int main(int argc, char *argv[]){
     args arguments;
     pthread_t threads[NUMBER_OF_THREADS];
-    int status, i;
+    int status, j;
     
     
-    for(i=0; i < NUMBER_OF_THREADS; i++){
-        printf("Hilo principal. Creando hilo %d\n", i);
+    for(j=0; j < NUMBER_OF_THREADS; j++){
+        printf("Hilo principal. Creando hilo %d\n", j);
         
-        arguments.tid = i; /* Guardamos el tid*/
+        arguments.tid = j; /* Guardamos el tid*/
         
-        status = pthread_create(&threads[i], NULL, thread_function, (void *)&arguments);
+        status = pthread_create(&threads[j], NULL, thread_function, (void *)&arguments);
         
         if(status != 0){
             printf("Error en la creación del hilo. Status: %d\n", status);
@@ -69,10 +36,27 @@ int main(int argc, char *argv[]){
         sleep(1);
     }
     
-    for(i=0; i < NUMBER_OF_THREADS; i++){
+    /*El hijo principal espera a la terminación de todos lo hilos */
+    for(j=0; j < NUMBER_OF_THREADS; j++){
         pthread_join(threads[i], NULL);
     }
     
 
     exit(EXIT_SUCCESS);
+}
+
+void * thread_function(void *arguments){
+    args *aux;
+    int j;
+    float sum = 0.0;
+
+     aux = (args *) arguments;
+
+    for(){
+    
+    }
+    
+   
+    pthread_exit(NULL);
+
 }
